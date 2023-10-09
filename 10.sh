0bd702239381b9787ef105cd1c1a72357895b7dd
MozumderHasan <md.mozumder.hasan@gmail.com>
2023-10-08 22:09:51 -0400

    add the global find command and change the specific file text

--- 10.sh
+++ 10.sh
@@ -1,1 +1,6 @@
+# this command run specific directory wise run
 sed -i 's/crash/broken/g' crash.in
+
+# This command find the location of file and change the specific text
+
+find . -name "crash.in" -exec sed -i 's/crash/broken/g' {} \;
